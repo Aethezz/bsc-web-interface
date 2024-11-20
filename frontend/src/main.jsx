2292f2bee94fb4d29cc15346c96938afccee22f5
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-11-20 17:16:27 -0500

    Components file added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Videos from './Videos.jsx'
-import Home from './home.jsx'
-import Contact from './Contact.jsx'
-import About from './About.jsx'
+import Videos from './Components/Videos.jsx'
+import Home from './Components/home.jsx'
+import Contact from './Components/Contact.jsx'
+import About from './Components/About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
